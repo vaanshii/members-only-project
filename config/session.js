86a5e6ce412c0e421d97854f5da79d04efb18a8a
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-04-09 16:46:22 -0700

    Configure session cookies middlewares.

---
+++ config/session.js
@@ -0,0 +1,14 @@
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./db/pool");
+require("dotenv").config();
+
+const sessionConfig = {
+	store: new pgSession({ pool, tableName: process.env.SESSION_TABLE_NAME }),
+	secret: process.env.COOKIE_SECRET,
+	resave: false,
+	saveUninitialized: true,
+	cookie: { maxAge: 1000 * 60 * 60 * 24 },
+};
+
+module.exports = session(sessionConfig);
