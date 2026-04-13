08060e5c4dfa5f45ece0d8df43721a3b3bb8359f
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-04-13 16:08:14 -0700

    Initialized passport middleware.

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,41 @@
 const passport = require("passport");
+const User = require("../models/User");
 const LocalStrategy = require("passport-local").Strategy;
+const { validatePassword } = require("../utils/passwordUtils");
+
+const verifyCallback = async (username, password, done) => {
+	try {
+		const user = await User.getByUsername(username);
+
+		if (!user) {
+			return done(null, false);
+		}
+
+		const isValid = validatePassword(password, user.password);
+		if (!isValid) {
+			return done(null, false);
+		}
+
+		return done(null, user);
+	} catch (error) {
+		console.error("[verifyCallback] Unexpected Error: ", error);
+		done(error);
+	}
+};
+
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.getUserById(id);
+		done(null, user);
+	} catch (error) {
+		done(error);
+	}
+});
