b5ae117a0bcb9597a056689f14e28f141077124a
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-04-14 15:05:23 -0700

    Add ejs setup and public files.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
-app.set("views", "ejs");
+app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
