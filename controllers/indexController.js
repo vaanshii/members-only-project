09908b6731ea4f659f0c9cad107c7c4b90262916
Ivan Russel <russelblanquiza@gmail.com>
2026-04-24 15:38:51 +0800

    Uses class name instead of deconstructing the class variable.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,4 @@
-const { Message } = require("../models/message");
+const Message = require("../models/message");
 const { formatDistanceToNow } = require("date-fns");
 
 async function getAllMessagesGET(req, res, next) {
