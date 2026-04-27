454f77e3c081606602643a1fc1a3b609f8b4c8e8
Ivan Russel <russelblanquiza@gmail.com>
2026-04-27 12:10:51 +0800

    Controller for updating the user info.

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,7 +1,9 @@
 const { formatDistanceToNow } = require("date-fns");
+const { validationResult, matchedData } = require("express-validator");
 
 const User = require("../models/user");
 const Message = require("../models/message");
+const { validateEditProfile } = require("../validators/editProfileValidator");
 
 async function getUserProfileGET(req, res, next) {
 	const username = req.params.username;
@@ -32,4 +34,56 @@ async function getUserProfileGET(req, res, next) {
 	}
 }
 
-module.exports = { getUserProfileGET };
+async function editProfileGET(req, res, next) {
+	const username = req.params.username;
+
+	try {
+		const userData = await User.getByUsername(username);
+
+		if (!req.isAuthenticated() || userData === undefined) {
+			return res.status(404).render("./partials/404", { title: "404" });
+		}
+
+		const user = {
+			firstName: userData.first_name,
+			lastName: userData.last_name,
+			motorcycle: userData.motorcycle,
+			bio: userData.bio,
+		};
+
+		return res
+			.status(200)
+			.render("editProfile", { errors: [], formData: user });
+	} catch (error) {
+		console.error("[editProfileGET] Error: ", error);
+		next(error);
+	}
+
+	return res.render("editProfile");
+}
+
+const updateProfilePOST = [
+	validateEditProfile,
+	async (req, res, next) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res
+				.status(401)
+				.render("editProfile", { errors: errors.array(), formData: req.body });
+		}
+
+		const userData = matchedData(req);
+		const username = req.user.username;
+
+		try {
+			const user = await User.updateInfoByUsername(userData, username);
+			res.redirect(`/profile/${username}`);
+		} catch (error) {
+			console.error("[updateProfilePOST] Error: ", error);
+			next(error);
+		}
+	},
+];
+
+module.exports = { getUserProfileGET, editProfileGET, updateProfilePOST };
