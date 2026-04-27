0fbcc49fa6a727c7f40c0933ddb9e418988efb94
Ivan Russel <russelblanquiza@gmail.com>
2026-04-27 12:11:50 +0800

    Added get routes for getting user  profile and post for updating profile.

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
-const { getUserProfileGET } = require("../controllers/profileController");
+const {
+	getUserProfileGET,
+	editProfileGET,
+	updateProfilePOST,
+} = require("../controllers/profileController");
 const { isAuth } = require("../middleware/authMiddleware");
 
 const profileRouter = Router();
 
 profileRouter.get("/:username", isAuth, getUserProfileGET);
+
+profileRouter.get("/:username/edit", isAuth, editProfileGET);
+
+profileRouter.post("/edit", isAuth, updateProfilePOST);
 
 module.exports = { profileRouter };
