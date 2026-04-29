5dc79d3c3e170951965ce5f4c4c735c37a6842c0
Ivan Russel <russelblanquiza@gmail.com>
2026-04-29 23:18:25 +0800

    Adds new route for password update.

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,7 +2,8 @@ const { Router } = require("express");
 const {
 	getUserProfileGET,
 	editProfileGET,
-	updateProfilePOST,
+	updateProfilePUT,
+	updatePasswordPATCH,
 } = require("../controllers/profileController");
 const { isAuth } = require("../middleware/authMiddleware");
 
@@ -12,6 +13,8 @@ profileRouter.get("/:username", isAuth, getUserProfileGET);
 
 profileRouter.get("/:username/edit", isAuth, editProfileGET);
 
-profileRouter.put("/edit", isAuth, updateProfilePOST);
+profileRouter.put("/edit", isAuth, updateProfilePUT);
+
+profileRouter.patch("/edit/password", isAuth, updatePasswordPATCH);
 
 module.exports = { profileRouter };
